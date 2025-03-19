b55cc343aa427577ba895cf07bfc62060eba2df2
OtsuruAyami <>
2025-03-19 13:22:18 +0900

    過程が全部残っているjsデータ

--- databse/assets/js/localStorage.js
+++ databse/assets/js/localStorage.js
@@ -1,19 +1,9 @@
-// console.log('localStorage test');
-/* if(('localStorage' in window) && (window.localStorage !== null)) {
-    // ローカルストレージが使える
-} else {
-    // 使えない。。。
-} */
-
 
 //sing in が押されたらデータを保存する。
 //＃emailの入力値と#passwordの入力値を
 
 //次回（ロードされたら）localStorageの情報を取得する。
 /* 
-const clickHandler = () => {
-
-}
  */
 document.getElementById('js-singin').addEventListener('click', ()=> {
     // console.log('サインインをクリック');
@@ -23,14 +13,27 @@ document.getElementById('js-singin').addEventListener('click', ()=> {
     localStorage.setItem('email', email);
     localStorage.setItem('password', password);
 
-    let emailOutput = localStorage.getItem('email');
-    let passwordOutput = localStorage.getItem('password');
+    //setItemできているか確認
+    // let emailOutput = localStorage.getItem('email');/*  */
+    // let passwordOutput = localStorage.getItem('password');
+    // console.log(emailOutput,passwordOutput);
+});
+
+/* let emailOutput = window.localStorage.getItem('email');
+let passwordOutput = window.localStorage.getItem('password');
+
+document.getElementById('email').textContent = emailOutput
+document.getElementById('password').textContent = passwordOutput
+
+ */
+// console.log(emailOutput,passwordOutput);
+
+window.addEventListener('load', () => {
+    let emailOutput = window.localStorage.getItem('email');
+    let passwordOutput = window.localStorage.getItem('password');
+
+    document.getElementById('email').value = emailOutput;
+    document.getElementById('password').value = passwordOutput;
 
     console.log(emailOutput,passwordOutput);
 });
-
-
-/* window.addEventListener('load', () => {
-    localStorage.getItem('email');
-    localStorage.getItem('password');
-}); */
